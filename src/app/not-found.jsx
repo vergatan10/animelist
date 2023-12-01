0bcd4659f2f5d827961bd990999632d094f97908
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-12-02 01:21:31 +0700

    fix bug router back

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,24 +1,24 @@
 "use client";
 import { FileSearch } from "@phosphor-icons/react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
+
   return (
-    // <div className="h-full flex justify-center items-center">
-    //   <h4 className="text-color-accent">Not found...</h4>
-    // </div>
     <div className="min-h-screen min-w-xl mx-auto flex justify-center items-center">
       <div className="flex justify-center items-center gap-4 flex-col">
         <FileSearch size={32} className="text-color-accent" />
         <h3 className="text-color-primary hover:text-color-accent transition-all text-2xl font-bold">
           Not Found...
         </h3>
-        <Link
-          href="/"
+        <button
+          href
+          onClick={() => router.back()}
           className="py-3 px-8 rounded-md bg-color-primary hover:text-color-accent hover:bg-color-secondary hover:ring-2 hover:ring-color-accent transition-all"
         >
           Kembali
-        </Link>
+        </button>
       </div>
     </div>
   );
