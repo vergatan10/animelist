0e2b250374963dad00d1ae87a78197fbdab4bdda
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-12-04 00:58:17 +0700

    update nextjs 14. add prisma, add collection

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,15 +1,27 @@
+import CollectionButton from "@/components/AnimeList/CollectionButton";
 import VideoPlayer from "@/components/Utilities/VideoPlayer";
 import { getAnimeResponse } from "@/libs/api-libs";
+import { authUserSession } from "@/libs/auth-libs";
+import prisma from "@/libs/prisma";
 import Image from "next/image";
 
 const Page = async ({ params: { id } }) => {
   const anime = await getAnimeResponse(`anime/${id}`);
+  const user = await authUserSession();
+
+  const collection = await prisma.collection.findFirst({
+    where: { user_email: user?.email, anime_mal_id: id },
+  });
+
   return (
     <>
       <div className="pt-4 px-4">
         <h3 className="text-2xl text-color-primary">
           {anime.data.title} - {anime.data.year}
         </h3>
+        {!collection && user && (
+          <CollectionButton anime_mal_id={id} user_email={user?.email} />
+        )}
       </div>
       <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
         <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
---
+++ src/app/api/v1/collection/route.js
@@ -0,0 +1,11 @@
+import prisma from "@/libs/prisma";
+
+export async function POST(req) {
+  const { anime_mal_id, user_email } = await req.json();
+  const data = { anime_mal_id, user_email };
+
+  const createCollection = await prisma.collection.create({ data });
+  if (!createCollection)
+    return Response.json({ status: 500, isCreated: false });
+  else return Response.json({ status: 201, isCreated: true, data: data });
+}
